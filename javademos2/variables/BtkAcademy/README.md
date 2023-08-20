fc5244056e8be409fb5a04c244a6aed9d5cadb83
Ozdemir <115483222+ozdemirkaya@users.noreply.github.com>
2023-08-20 20:16:47 +0300

    Add files via upload

---
+++ javademos2/variables/BtkAcademy/README.md
@@ -0,0 +1,2 @@
+# BtkAcademy
+ 
---
+++ javademos2/variables/variables2/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int sayi = 12;
+        String mesaj = "öğrenci sayısı : ";
+
+        System.out.println(mesaj+sayi);
+    }
+
+}
